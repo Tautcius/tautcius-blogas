5aed2f2d34dacb01a71486fe477444f846e4cc5a
Tautcius <t.bujauskas@gmail.com>
2019-04-10 21:50:48 +0300

    font change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,30 @@
 module.exports = {
   theme: {
-    // Some useful comment
+    fontFamily: {
+      sans: [
+        "Roboto",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        '"Helvetica Neue"',
+        "Arial",
+        '"Noto Sans"',
+        "sans-serif",
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"'
+      ],
+      serif: ["Georgia", "Cambria", '"Times New Roman"', "Times", "serif"],
+      mono: [
+        "Menlo",
+        "Monaco",
+        "Consolas",
+        '"Liberation Mono"',
+        '"Courier New"',
+        "monospace"
+      ]
+    }
   },
   variants: {
     // Some useful comment
@@ -8,4 +32,4 @@ module.exports = {
   plugins: [
     // Some useful comment
   ]
-}
+};
