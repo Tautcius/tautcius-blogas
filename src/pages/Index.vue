d2d721993cc112fc12aebbbb41b97993a83eb6c3
Tautcius <t.bujauskas@gmail.com>
2019-04-26 10:26:26 +0000

    typing done by gridsome home page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,8 +3,8 @@
     <div class="container mx-auto -mt-20 h-full flex justify-center items-center font-mono">
        <div class="w-2/3">
     <h1 class="text-5xl p-10">Tautciaus Blogas</h1>
-    <h2 class="text-3xl p-6 ml-10">Apie <vue-typer
-  :text='["kompiuterius","programavimą","internetą"]'
+    <h2 class="text-3xl p-6 ml-10">Apie <VueTyper
+  :text='words'
   :repeat='Infinity'
   :shuffle='false'
   initial-action='typing'
@@ -14,8 +14,7 @@
   :erase-delay='100'
   erase-style='backspace'
   :erase-on-complete='false'
-  caret-animation='blink'
-></vue-typer>
+  caret-animation='blink' />
     </h2>
     <h2 class="text-3xl p-6 ml-10">ir viską kas man įdomu.</h2>
     </div>
@@ -24,9 +23,21 @@
 </template>
 
 <script>
+const words = ["kompiuterius","programavimą","internetą"];
+
 export default {
     metaInfo: {
     title: "Pagrindinis"
+  },
+  components: {
+          VueTyper: process.isServer
+      ? { inheritAttrs: false, render: h => h('span', null, [words[0]]) }
+      : () => import('vue-typer').then(({ VueTyper }) => VueTyper)
+  },
+  data () {
+      return {
+          words
+      }
   }
 };
 </script>
