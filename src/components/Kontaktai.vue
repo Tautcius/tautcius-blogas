87172500420dfe740abbd5d68b70fed5ba622939
Tautcius <t.bujauskas@gmail.com>
2019-05-29 17:04:57 +0300

    fixed responsice profile photo.

--- src/components/Kontaktai.vue
+++ src/components/Kontaktai.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="w-full lg:flex">
-  <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style="background-image: url('https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/44680909_2482320701793295_2502996709773148160_n.jpg?_nc_cat=100&_nc_ht=scontent-arn2-1.xx&oh=29c1c83e6cc39d00a67d280218c8367d&oe=5D3601C8')" title="Woman holding a mug">
+  <div class="custom lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style="background-image: url('https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/44680909_2482320701793295_2502996709773148160_n.jpg?_nc_cat=100&_nc_ht=scontent-arn2-1.xx&oh=29c1c83e6cc39d00a67d280218c8367d&oe=5D3601C8')">
   </div>
   <div class="border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
     <div class="mb-8">
@@ -24,4 +24,7 @@ export default {
 }
 </script>
 <style>
+.custom {
+  height: 400px
+}
 </style>
